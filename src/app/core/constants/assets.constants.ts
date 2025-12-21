7f47769a451b7758717aefa4741bc1cbeb218205
Oscar ordoñez <oscargonz150@gmail.com>
2025-12-21 12:32:50 -0600

    ✨ | footer | img | components
    Se modifico el footer para agregar img decorativas
    y se modifico para generar componetes styles de tailwind para ser reutilizados

--- src/app/core/constants/assets.constants.ts
+++ src/app/core/constants/assets.constants.ts
@@ -1,5 +1,6 @@
 const IMAGES_BASE_PATH = 'assets/images';
 const VIDEOS_BASE_PATH = 'assets/videos';
+const ANIMES_PATH = 'assets/anime';
 
 export const ASSETS_PATHS = {
   videos: {
@@ -12,6 +13,17 @@ export const ASSETS_PATHS = {
     sudadera: {
       sudadera1: `${IMAGES_BASE_PATH}/sudadera1.jpg`,
     },
+    categorias: {
+      deportes: `${IMAGES_BASE_PATH}/deportes.jpg`,
+    },
+  },
+  anime: {
+    jujutsu: `${ANIMES_PATH}/jujutsu.png`,
+    dandadan: `${ANIMES_PATH}/dandadan.webp`,
+    invincible: `${ANIMES_PATH}/invencible.jpeg`,
+    onepiece: `${ANIMES_PATH}/one.jpeg`,
+    naruto: `${ANIMES_PATH}/naruto.jpg`,
+    deportes: `${ANIMES_PATH}/deportes.jpg`,
   },
 } as const;
 
--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ASSETS_PATHS } from '../../../core/constants/assets.constants';
 
 @Component({
   selector: 'app-footer',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
   email = '';
+  ASSETS_PATHS = ASSETS_PATHS;
 
   subscribe(): void {
     if (this.email) {
